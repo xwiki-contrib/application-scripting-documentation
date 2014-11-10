f08eaa6fd6d08878fb669e499d20fb23de49c38e
Denis Gervalle <dgl@softec.lu>
2014-11-10 17:29:02 +0100

    Fix copyright notice

--- scripting-documentation-api/src/main/java/org/xwiki/scripting/documentation/internal/AbstractBindingResource.java
+++ scripting-documentation-api/src/main/java/org/xwiki/scripting/documentation/internal/AbstractBindingResource.java
@@ -1,14 +1,21 @@
 /*
- * Copyright 2010-2014 SOFTEC sa. All rights reserved.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
  *
- * This software is copyrighted. The software may not be copied,
- * reproduced, translated, or reduced to any electronic medium
- * or machine-readable form without the prior written consent of
- * SOFTEC sa.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO SOFTEC S.A.
- * AND MAY NOT BE REPRODUCED, PUBLISHED, OR DISCLOSED TO OTHERS
- * WITHOUT COMPANY AUTHORIZATION.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
 package org.xwiki.scripting.documentation.internal;
